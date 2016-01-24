72688b9983e3c762bbb564f1e926c5176d709505
John Grib <johngrib82@gmail.com>
2016-01-24 23:35:10 +0900

    sort_func 수정

--- footloosegrid.4.js
+++ footloosegrid.4.js
@@ -2028,23 +2028,29 @@ function _create_buttons(_this){
  */
 FGR.prototype.sort_func = function(col){
   return {
-    number_asc  : function(a,b){
+    number_asc (a,b) {
       if(a[col] === null) return 1;
       if(b[col] === null) return -1;
-      return  a[col] - b[col]; },
-    number_desc : function(a,b){
+      return  a[col] - b[col];
+    },
+    number_desc (a,b) {
       if(a[col] === null) return 1;
       if(b[col] === null) return -1;
-      return -a[col] + b[col]; },
-    str_asc     : function(a,b){
+      return -a[col] + b[col];
+    },
+    str_asc (a,b) {
       if(a[col] === null || a[col] === '') return 1;
       if(b[col] === null || b[col] === '') return -1;
-      return (a[col] < b[col]) ? -1 : 1; },
-    str_desc    : function(a,b){
+      return (a[col] < b[col]) ? -1 : 1;
+    },
+    str_desc (a,b) {
       if(a[col] === null || a[col] === '') return 1;
       if(b[col] === null || b[col] === '') return -1;
-      return (a[col] > b[col]) ? -1 : 1; },
-    revert      : function(a,b){return  a.index - b.index;}
+      return (a[col] > b[col]) ? -1 : 1;
+    },
+    revert (a,b) {
+      return  a.index - b.index;
+    },
   };
 };
 
